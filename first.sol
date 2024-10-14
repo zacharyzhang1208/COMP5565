c3a1573f2c8f1d1092316a8a5e402b17027140b8
zacharyzhang1208 <zacharyzhang1208@hotmail.com>
2024-10-14 17:05:42 +0800

    add event Set

--- first.sol
+++ first.sol
@@ -4,18 +4,21 @@ contract SimpleStorage {
     uint256 public storedData;
     address public owner;
 
+    event Set(string message);
+
     constructor() {
         storedData = 10;
         owner = msg.sender;
     }
 
     modifier only_owner() {
-        require(msg.sender == owner,"you are not the owner");
+        require(msg.sender == owner,"You are not the owner");
         _;
     }
 
     function set(uint256 x) public only_owner{
     storedData = x;
+    emit Set("New value set.");
     }
 
     function get() public view returns (uint256) {
