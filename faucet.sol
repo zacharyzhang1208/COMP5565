40fcbee125b39a39bc996461003c2071cbc28d10
zacharyzhang1208 <zacharyzhang1208@hotmail.com>
2024-11-01 20:04:43 +0800

    add definition of getAllAddresses()

--- faucet.sol
+++ faucet.sol
@@ -3,26 +3,33 @@ pragma solidity ^0.8.15;
 
 // Our first contract is a faucet contract Faucet {
 contract Faucet {
-    // Our first contract is a faucet contract Faucet {
     address owner;
+    uint256[] addresses;
+    event Log(string message);
+
     constructor() {
-    owner = msg.sender;
-    }
-    modifier only_owner() {
-        require(msg.sender == owner, "Not the owner!");
-        _;
+        owner = msg.sender;
     }
 
     // Give out ether to anyone who asks
-    function withdraw(uint256 withdraw_amount) public only_owner {
+    function withdraw(uint256 withdraw_amount) public {
         // Limit withdrawal amount
         require(withdraw_amount <= 1 ether);
         // Send the amount to the address that request it
         payable(msg.sender).transfer(withdraw_amount);  
+        emit Log("withdraw happens");
+        addresses.push(withdraw_amount);
     }
-    function transfer(address payable _to, uint256 _amount) public only_owner {
+
+    function transfer(address payable _to, uint256 _amount) public {
         _to.transfer(_amount);
     }
 
-    receive() external payable {}
+    function getAllAddresses() public view returns (address[] memory){
+        
+    }
+
+    receive() external payable {
+
+    }
 }
